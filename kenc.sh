839f2b846fc0b934615d96118d4c68d59bc4c141
tiancao2022 <tiancaochenguang@163.com>
2022-08-14 21:57:57 +0800

    LXZG

--- kenc.sh
+++ kenc.sh
@@ -2,7 +2,7 @@
 # Author: LX007
 # github: https://github.com/tiancao2022/LXMinerProxy
 
-DOWNLOAD_HOST=""
+DOWNLOAD_HOST="https://github.com/tiancao2022/LXMinerProxy/raw/master/Linux-64"
 
 AMD64_HOST="https://github.com/tiancao2022/LXMinerProxy/raw/master/KENC/kenc_linux_amd64"
 
