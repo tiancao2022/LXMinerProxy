d59f431ca31b387ac444a3f53b7282bdfd7fef95
tiancao2022 <tiancaochenguang@163.com>
2022-10-26 23:31:00 +0800

    LXZG

--- linux-install.sh
+++ linux-install.sh
@@ -12,8 +12,6 @@ PATH_LX="/root/lxmproxy"
 
 PATH_EXEC="lxproxy"
 
-PATH_CU="/root/lxmproxy/cu.kt"
-
 PATH_CACHE="/root/lxmproxy/.cache"
 
 PATH_LICENSE="/root/lxmproxy/license"
@@ -308,9 +306,8 @@ installapp() {
     colorEcho $BLUE "拉取程序"
     # wget -P $PATH_LX "${DOWNLOAD_HOST}/${ORIGIN_EXEC}" -O "${PATH_LX}/${PATH_EXEC}" 1>/dev/null
     wget -P $PATH_LX "${DOWNLOAD_HOST}/lxproxy_v${VERSION}_linux" -O "${PATH_LX}/${PATH_EXEC}" 1>/dev/null
-  wget -P $PATH_LX "${DOWNLOAD_HOST}" -O "${PATH_LX}/${PATH_CU}" 1>/dev/null
-   
- filterResult $? "拉取程序 lxproxy_v${VERSION}_linux"
+
+    filterResult $? "拉取程序 lxproxy_v${VERSION}_linux"
 
     chmod 777 -R "${PATH_LX}/${PATH_EXEC}"
 
